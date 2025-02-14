c7429ce800d00f9e4f5b3638af23919d2bb36fb6
nmfirdausw <nmfirdausw@gmail.com>
2025-02-14 12:28:42 +0800

    feat: add treesitter plugins

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,24 @@
+MiniDeps.now(func
+  MiniDeps.add({
+    source = 'nvim-treesitter/nvim-treesitter',
+		hooks = {
+			post_checkout = function()
+				vim.cmd('TSUpdate')
+			end
+		},
+  })
+end)
+
+require('nvim-treesitter.configs').setup({
+  ensure_installed = {
+		'lua',
+		'markdown',
+		'markdown_inline',
+		'query',
+		'vim',
+		'vimdoc',
+	},
+  highlight = {
+    enable = true
+  },
+})
