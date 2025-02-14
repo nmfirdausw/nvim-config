db989e6f3785b4c1224ac2a84eb3b40bdb9b0cd2
nmfirdausw <nmfirdausw@gmail.com>
2025-02-14 11:39:26 +0800

    feat: add indentation opt config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,10 @@
 vim.opt.rtp:append('/opt/homebrew/opt/fzf')
 
 vim.opt.clipboard = "unnamedplus"
+
+-- Indentation
+vim.opt.expandtab = true
+vim.opt.shiftround = true
+vim.opt.shiftwidth = 2
+vim.opt.smartindent = true
+vim.opt.tabstop = 2
