7e236574535baf8575209b5e134052033fac2ba7
nmfirdausw <nmfirdausw@gmail.com>
2025-02-14 22:29:54 +0800

    chore: add condition to switch icons to devicons or not

---
+++ lua/plugins/icons.lua
@@ -0,0 +1,17 @@
+MiniDeps.now(function()
+  MiniDeps.add({
+    source = "nvim-tree/nvim-web-devicons",
+  })
+
+  if not Icons.devicons then
+    MiniDeps.add({
+      source = "projekt0n/circles.nvim",
+      depends = { "kyazdani42/nvim-web-devicons" },
+    })
+
+    require("circles").setup({
+      icons = { empty = Icons.file, filled = Icons.file, lsp_prefix = Icons.file },
+      lsp = false,
+    })
+  end
+end)
