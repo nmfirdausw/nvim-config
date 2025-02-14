2826035137b1d997e5bc14c1b670e6ee8f6f30b7
nmfirdausw <nmfirdausw@gmail.com>
2025-02-14 18:40:33 +0800

    feat: add which key

---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,17 @@
+MiniDeps.later(function()
+  MiniDeps.add({
+    source = "folke/which-key.nvim",
+  })
+  require("which-key").setup({
+    preset = "helix",
+    win = {
+      no_overlap = true,
+      col = 0,
+    },
+    icons = {
+      mappings = false,
+    },
+    spec = {
+    },
+  })
+end)
