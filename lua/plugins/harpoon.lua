a7db23643d3cab9bcb3a268b32b7b133a55669a9
nmfirdausw <nmfirdausw@gmail.com>
2025-04-04 06:56:57 +0800

    feat: add Harpoon for quick file navigation
    Implement ThePrimeagen's Harpoon v2 for fast file switching and navigation.
    Configure keyboard shortcuts including leader keys and custom keybindings
    for accessing frequently used files.

---
+++ lua/plugins/harpoon.lua
@@ -0,0 +1,76 @@
+return {
+  "ThePrimeagen/harpoon",
+  branch = "harpoon2",
+  opts = {
+    menu = {
+      width = vim.api.nvim_win_get_width(0) - 4,
+    },
+    settings = {
+      save_on_toggle = true,
+    },
+  },
+  keys = function()
+    local keys = {
+      {
+        "<leader>H",
+        function()
+          require("harpoon"):list():add()
+        end,
+        desc = "Harpoon File",
+      },
+      {
+        "<leader>h",
+        function()
+          local harpoon = require("harpoon")
+          harpoon.ui:toggle_quick_menu(harpoon:list())
+        end,
+        desc = "Harpoon Quick Menu",
+      },
+      {
+        "<C-l>",
+        function()
+          require("harpoon"):list():select(1)
+        end,
+        desc = "Harpoon to File 1",
+      },
+      {
+        "<C-y>",
+        function()
+          require("harpoon"):list():select(2)
+        end,
+        desc = "Harpoon to File 2",
+      },
+      {
+        "<C-p>",
+        function()
+          require("harpoon"):list():select(3)
+        end,
+        desc = "Harpoon to File 3",
+      },
+      {
+        "<C-i>",
+        function()
+          require("harpoon"):list():prev()
+        end,
+        desc = "Harpoon previous",
+      },
+      {
+        "<C-a>",
+        function()
+          require("harpoon"):list():next()
+        end,
+        desc = "Harpoon next",
+      },
+    }
+    for i = 1, 5 do
+      table.insert(keys, {
+        "<leader>" .. i,
+        function()
+          require("harpoon"):list():select(i)
+        end,
+        desc = "Harpoon to File " .. i,
+      })
+    end
+    return keys
+  end,
+}
