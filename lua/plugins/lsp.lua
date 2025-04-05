dfea6608068d9ab0f6ce09db99a4a68b02d88207
nmfirdausw <nmfirdausw@gmail.com>
2025-04-06 00:08:04 +0800

    feat: add comprehensive LSP configuration
    Implement Language Server Protocol setup:
    - Configure Mason for automatic installation of language servers
    - Set up LSP servers for multiple languages (PHP, HTML, CSS, TypeScript, etc.)
    - Add support for Laravel Blade templates across multiple language servers
    - Configure JSON schema validation with schemastore integration
    - Set up diagnostic display with custom icons
    - Add keybindings for LSP actions (hover, definition, references, etc.)
    - Configure Tailwind CSS support for web development

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,283 @@
+return {
+	{
+		"folke/lazydev.nvim",
+		ft = "lua",
+		opts = {
+			library = {
+				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+			},
+		},
+	},
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		dependencies = {
+			{
+				"williamboman/mason-lspconfig.nvim",
+				cmd = { "LspInstall", "LspUninstall" },
+			},
+			{
+				"williamboman/mason.nvim",
+				cmd = "Mason",
+				build = ":MasonUpdate",
+				opts = {
+					ui = {
+						border = "rounded",
+						height = 0.8,
+					},
+				},
+			},
+		},
+		lazy = false,
+		opts_extend = { "ensure_installed" },
+		opts = {
+			ensure_installed = {
+				-- lsp
+				"css-lsp",
+				"emmet-ls",
+				"docker-compose-language-service",
+				"dockerfile-language-server",
+				"html-lsp",
+				"intelephense",
+				"json-lsp",
+				"lua-language-server",
+				"python-lsp-server",
+				"tailwindcss-language-server",
+				"typescript-language-server",
+				"yaml-language-server",
+				-- linter
+				"duster",
+				"tlint",
+				"phpstan",
+				"phpcs",
+				-- formatter
+				"blade-formatter",
+				"stylua",
+				"shfmt",
+				"biome",
+				"duster",
+				"pint",
+				"yamlfmt",
+				"php-cs-fixer",
+				-- dap
+				"php-debug-adapter",
+			},
+			auto_update = true,
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"b0o/schemastore.nvim",
+		},
+		event = { "BufReadPost", "BufNewFile", "BufWritePost" },
+		opts = function()
+			return {
+				diagnostics = {
+					underline = true,
+					update_in_insert = true,
+					virtual_text = {
+						spacing = 4,
+						source = "if_many",
+					},
+				},
+				servers = {
+					emmet_ls = {
+						filetypes = {
+							"blade",
+							"html",
+							"css",
+						},
+					},
+					html = {
+						filetypes = {
+							"blade.html.php",
+							"blade",
+							"html",
+						},
+					},
+					jsonls = {
+						settings = {
+							json = {
+								schemas = require("schemastore").json.schemas(),
+								validate = {
+									enable = true,
+								},
+							},
+						},
+					},
+					tailwindcss = {
+						filetypes = {
+							"blade",
+							"html",
+							"css",
+						},
+						settings = {
+							tailwindCSS = {
+								emmetCompletions = true,
+								experimental = {
+									classRegex = false,
+								},
+								classAttributes = {
+									"class",
+									"@class",
+									"className",
+									"classList",
+								},
+							},
+						},
+						ts_ls = {
+							filetypes = {
+								"typescript",
+								"javascript",
+								"json",
+								"blade",
+								"html",
+							},
+						},
+					},
+				},
+			}
+		end,
+		config = function(_, opts)
+			local lspconfig = require("lspconfig")
+			local blink = require("blink.cmp")
+			local capabilities = vim.tbl_deep_extend(
+				"force",
+				{},
+				vim.lsp.protocol.make_client_capabilities(),
+				blink.get_lsp_capabilities() or {}
+			)
+			local handlers = {
+				function(server)
+					lspconfig[server].setup({
+						capabilities = capabilities,
+					})
+				end,
+			}
+
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
+				callback = function(event)
+					if vim.g.inlay_hint then
+						vim.lsp.inlay_hint.enable()
+					end
+					vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", { desc = "Hover", buffer = event.buf })
+					vim.keymap.set(
+						"n",
+						"gd",
+						"<cmd>lua vim.lsp.buf.definition()<cr>",
+						{ desc = "Goto definition", buffer = event.buf }
+					)
+					vim.keymap.set(
+						"n",
+						"gD",
+						"<cmd>lua vim.lsp.buf.declaration()<cr>",
+						{ desc = "Goto declaration", buffer = event.buf }
+					)
+					vim.keymap.set(
+						"n",
+						"gi",
+						"<cmd>lua vim.lsp.buf.implementation()<cr>",
+						{ desc = "Goto implementation", buffer = event.buf }
+					)
+					vim.keymap.set(
+						"n",
+						"go",
+						"<cmd>lua vim.lsp.buf.type_definition()<cr>",
+						{ desc = "Goto type definition", buffer = event.buf }
+					)
+					vim.keymap.set(
+						"n",
+						"gr",
+						"<cmd>lua vim.lsp.buf.references()<cr>",
+						{ desc = "Goto references", buffer = event.buf }
+					)
+					vim.keymap.set(
+						"n",
+						"gs",
+						"<cmd>lua vim.lsp.buf.signature_help()<cr>",
+						{ desc = "Signature help", buffer = event.buf }
+					)
+					vim.keymap.set(
+						"n",
+						"gR",
+						"<cmd>lua vim.lsp.buf.rename()<cr>",
+						{ desc = "Rename", buffer = event.buf }
+					)
+					-- vim.keymap.set({ 'n', 'x' }, 'gF', '<cmd>lua vim.lsp.buf.format({async = true})<cr>',
+					-- { desc = "Format", buffer = event.buf })
+					vim.keymap.set(
+						"n",
+						"ga",
+						"<cmd>lua vim.lsp.buf.code_action()<cr>",
+						{ desc = "Code action", buffer = event.buf }
+					)
+				end,
+			})
+
+			for server, server_opts in pairs(opts.servers) do
+				opts = vim.tbl_extend("force", {
+					capabilities = vim.deepcopy(capabilities),
+				}, server_opts)
+				handlers[server] = function()
+					lspconfig[server].setup(server_opts)
+				end
+			end
+
+			require("mason-lspconfig").setup_handlers(handlers)
+
+			local icons = {}
+
+			if vim.g.icons then
+				icons = {
+					signs = {
+						diagnostics = {
+							error = Icons.signs.diagnostics.error,
+							warn = Icons.signs.diagnostics.warn,
+							hint = Icons.signs.diagnostics.hint,
+							info = Icons.signs.diagnostics.info,
+						},
+					},
+				}
+			else
+				icons = {
+					signs = {
+						diagnostics = {
+							error = "󰑊",
+							warn = "󰑊",
+							hint = "󰑊",
+							info = "󰑊",
+						},
+					},
+				}
+			end
+
+			local diagnostics = {
+				virtual_text = {
+					prefix = function(diagnostic)
+						for severity, icon in pairs(Icons.virtual_text.diagnostics) do
+							if diagnostic.severity == vim.diagnostic.severity[severity:upper()] then
+								return icon
+							end
+						end
+					end,
+				},
+			}
+
+			vim.diagnostic.config({
+				signs = {
+					text = {
+						[vim.diagnostic.severity.ERROR] = icons.signs.diagnostics.error,
+						[vim.diagnostic.severity.WARN] = icons.signs.diagnostics.warn,
+						[vim.diagnostic.severity.HINT] = icons.signs.diagnostics.hint,
+						[vim.diagnostic.severity.INFO] = icons.signs.diagnostics.info,
+					},
+				},
+				underline = true,
+				update_in_insert = true,
+				severity_sort = true,
+				virtual_text = diagnostics.virtual_text,
+			})
+		end,
+	},
+}
