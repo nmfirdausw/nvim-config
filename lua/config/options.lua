2fad30dd019466c172e00ca7795898c5a7a79c6a
nmfirdausw <nmfirdausw@gmail.com>
2025-08-06 23:55:50 +0800

    feat: configure file handling and history settings
    - Disable backup, writebackup, and swap files for cleaner workspace
    - Enable persistent undo with 1000 undo levels
    - Set command history to 1000 entries
    - Enable confirmation prompts for destructive operations

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,3 +6,12 @@ vim.opt.tabstop = 2
 -- Behaviour
 vim.opt.splitright = true
 vim.opt.splitbelow = true
+
+-- History and backup
+vim.opt.backup = false
+vim.opt.confirm = true
+vim.opt.history = 1000
+vim.opt.swapfile = false
+vim.opt.undofile = true
+vim.opt.undolevels = 1000
+vim.opt.writebackup = false
