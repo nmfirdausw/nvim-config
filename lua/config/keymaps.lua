1b187625b6cf1a5a8ee0e30af0a271f06d37d21d
nmfirdausw <nmfirdausw@gmail.com>
2025-08-12 02:48:39 +0800

    feat(keymaps): add buffer delete keymap
    - Add <leader>bd keymap to delete current buffer using :bdelete
    - Integrates with existing which-key buffer group
    - Provides safe buffer deletion with unsaved changes warning

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -17,10 +17,18 @@ require("which-key").setup({
     { "<leader>b", desc = "Buffer" },
     { "<leader>[", desc = "Previous", cond = Utils.is_git_dir() },
     { "<leader>]", desc = "Next", cond = Utils.is_git_dir() },
-    { "<leader>g", desc = "Git", cond = Utils.is_git_dir() },
+    { "<leader>g", desc = "Git", cond = Utils.is_git_dir(), mode = { "n", "v" } },
     { "<leader>gb", desc = "Blame", cond = Utils.is_git_dir() },
     { "<leader>gd", desc = "Diff", cond = Utils.is_git_dir() },
     { "<leader>gh", desc = "Hunk", cond = Utils.is_git_dir() },
     { "<leader>tg", desc = "Git", cond = Utils.is_git_dir() },
   },
 })
+
+-- Buffer keymaps
+vim.keymap.set("n", "<leader>bx", ":bdelete<CR>", { desc = "Delete" })
+
+-- System clipboard keymaps
+vim.keymap.set("n", "<leader>y", "\"+yy", { desc = "Copy line to system clipboard" })
+vim.keymap.set("v", "<leader>y", "\"+y", { desc = "Copy selection to system clipboard" })
+vim.keymap.set("n", "<leader>p", "\"+p", { desc = "Paste from system clipboard" })
