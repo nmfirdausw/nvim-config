214889124a577afa9fcba5c2ead78d3ecd93eca7
nmfirdausw <nmfirdausw@gmail.com>
2025-09-08 16:57:21 +0800

    feat: add friendly-snippets dependency
    - Add rafamadriz/friendly-snippets for comprehensive snippet collection
    - Prepare dependency for completion system integration

--- lua/config/dependencies.lua
+++ lua/config/dependencies.lua
@@ -1,4 +1,5 @@
 vim.pack.add({
   { src = "https://github.com/kevinhwang91/promise-async" },
   { src = "https://github.com/nvim-tree/nvim-web-devicons" },
+  { src = "https://github.com/rafamadriz/friendly-snippets" },
 })
