88bb9ae0d1f2f9a9e7900e68f6367dd578cd557f
nmfirdausw <nmfirdausw@gmail.com>
2025-09-10 21:24:50 +0800

    feat(keymaps): add enhanced navigation and scrolling keybindings
    - Add Shift+Arrow navigation: Shift+Down/Up for paragraph movement (}/{ )
    - Add Shift+Left/Right for word movement backward/forward (B/W)
    - Add centered scrolling with Ctrl+d/u that includes zz for screen centering
    - Improves navigation ergonomics and keeps cursor centered during page scrolling

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -29,3 +29,12 @@ require("which-key").setup({
 vim.keymap.set("n", "<leader>y", "\"+yy", { desc = "Copy line to clipboard" })
 vim.keymap.set("v", "<leader>y", "\"+y", { desc = "Copy selection to clipboard" })
 vim.keymap.set("n", "<leader>p", "\"+p", { desc = "Paste from clipboard" })
+
+vim.keymap.set("n", "<S-down>", "}")
+vim.keymap.set("n", "<S-up>", "{")
+vim.keymap.set("n", "<S-left>", "B")
+vim.keymap.set("n", "<S-right>", "W")
+
+-- add zz to scle up/down
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
