704e5fddf4b1c9fdd5d55792218449bf378bc35f
nmfirdausw <nmfirdausw@gmail.com>
2025-09-12 22:20:40 +0800

    refactor: remove old configuration files migrated to lazy plugins
    - Remove completion.lua (migrated to lua/plugins/blink-cmp.lua)
    - Remove dependencies.lua (dependencies now managed by lazy.nvim)
    - Remove folding.lua (migrated to lua/plugins/ufo.lua and config/fold.lua)
    - Remove git.lua (migrated to lua/plugins/gitsigns.lua)

--- lua/config/dependencies.lua
+++
@@ -1,5 +0,0 @@
-vim.pack.add({
-  { src = "https://github.com/kevinhwang91/promise-async" },
-  { src = "https://github.com/nvim-tree/nvim-web-devicons" },
-  { src = "https://github.com/rafamadriz/friendly-snippets" },
-})
