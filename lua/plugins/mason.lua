f88699bab96c2e5b85cbbcf80ef898cd890d4dfa
nmfirdausw <nmfirdausw@gmail.com>
2025-09-12 10:51:29 +0800

    refactor: migrate mason configuration to lazy plugin system
    - Move mason setup from config/ to plugins/ for lazy loading
    - Convert to lazy.nvim plugin specification format
    - Disable auto-update and run_on_start for better control
    - Update lock file to reflect new plugin structure

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,55 @@
+return {
+  "WhoIsSethDaniel/mason-tool-installer.nvim",
+  cmd = {
+    "MasonToolsClean",
+    "MasonToolsInstall",
+    "MasonToolsInstallSync",
+    "MasonToolsUpdate",
+    "MasonetoolsUpdateSync",
+  },
+  dependencies = {
+    "mason-org/mason.nvim",
+    cmd = {
+      "Mason",
+      "MasonInstall",
+      "MasonUninstall",
+      "MasonUninstallAll",
+      "MasonLog",
+      "MasonUpdate",
+    },
+    opts = {
+      registries = {
+        "github:mason-org/mason-registry",
+        "github:Crashdummyy/mason-registry",
+      },
+      ui = {
+        border = vim.g.border,
+        icons = {
+          package_installed = "+",
+          package_pending = "!",
+          package_uninstalled = "x",
+        },
+      },
+    },
+  },
+  opts = {
+    ensure_installed = {
+      "angular-language-server",
+      "biome",
+      "blade-formatter",
+      "css-lsp",
+      "css-variables-language-server",
+      "cssmodules-language-server",
+      "html-lsp",
+      "lua-language-server",
+      "prettierd",
+      "roslyn",
+      "rzls",
+      "stylua",
+      "typescript-language-server",
+    },
+    auto_update = false,
+    run_on_start = false,
+    start_delay = 3000,
+  },
+}
