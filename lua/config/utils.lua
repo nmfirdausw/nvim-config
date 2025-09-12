117daf67aadccf08b89ad7975050d03f4642fd77
nmfirdausw <nmfirdausw@gmail.com>
2025-09-13 01:45:01 +0800

    config: update utils.lua configuration
    - Enhance utility functions and helpers
    - Improve global utility access and functionality

--- lua/config/utils.lua
+++ lua/config/utils.lua
@@ -1,12 +1,5 @@
 local M = {}
 
--- Clean error messages by removing file paths and line numbers
-function M.clean_error(err)
-  local clean_err = tostring(err):match(": (.*)$") or tostring(err)
-  clean_err = clean_err:gsub("^%d+: ", "") -- Remove line number prefix
-  return clean_err
-end
-
 M.is_git_dir = function()
   local cwd = vim.fn.getcwd():gsub(" ", "\\ ")
   local gitdir = vim.fn.finddir(".git", cwd .. ";")
