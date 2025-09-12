51ba4a9897bfc4c2e54a0312627c7a79044eb8c9
nmfirdausw <nmfirdausw@gmail.com>
2025-09-12 20:59:00 +0800

    refactor: remove old formatting and linting config files
    - Remove lua/config/formatting.lua (replaced by modular structure)
    - Remove lua/config/linting.lua (replaced by modular structure)

--- lua/config/linting.lua
+++
@@ -1,9 +0,0 @@
-vim.pack.add({ "https://github.com/mfussenegger/nvim-lint" })
-
-require("lint").linters_by_ft = {}
-
-vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-  callback = function()
-    require("lint").try_lint()
-  end,
-})
