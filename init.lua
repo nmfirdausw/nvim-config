facdfbf9de9321515045850055d2990b335790d8
nmfirdausw <nmfirdausw@gmail.com>
2025-09-12 20:57:09 +0800

    config: add PATH setup and diagnostics module

--- init.lua
+++ init.lua
@@ -1,6 +1,9 @@
+vim.env.PATH = "/opt/homebrew/bin:" .. vim.env.PATH
+
 _G.Utils = require("config.utils")
 
 require("config.options")
 require("config.neovide")
 require("config.keymaps")
+require("config.diagnostics")
 require("config.lazy")
