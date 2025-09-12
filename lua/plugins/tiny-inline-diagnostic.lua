be87a947abeb72a2fe1388deea32b1fe30b2264f
nmfirdausw <nmfirdausw@gmail.com>
2025-09-13 01:44:11 +0800

    config: simplify tiny-inline-diagnostic preset and options
    - Set preset to "simple" for cleaner display
    - Disable show_source for reduced visual clutter
    - Keep throttle at 0 for immediate feedback

--- lua/plugins/tiny-inline-diagnostic.lua
+++ lua/plugins/tiny-inline-diagnostic.lua
@@ -4,9 +4,10 @@ return {
   priority = 1000,
   config = function()
     require("tiny-inline-diagnostic").setup({
+      preset = "simple",
       options = {
         show_source = {
-          enabled = true,
+          enabled = false,
         },
         use_icons_from_diagnostic = true,
         throttle = 0,
