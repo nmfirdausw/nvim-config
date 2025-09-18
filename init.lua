722c4cfaf9337b35e8576d53e418fdc3c8457838
nmfirdausw <nmfirdausw@gmail.com>
2025-09-18 21:34:46 +0800

    feat: improve golden ratio picker positioning
    - Calculate picker height using golden ratio for better visual balance
    - Update PickerPosH to use calculated height offset
    - Maintain consistent golden ratio proportions across window layouts

--- init.lua
+++ init.lua
@@ -1,9 +1,12 @@
 vim.env.PATH = "/opt/homebrew/bin:" .. vim.env.PATH
 
+local lines = vim.opt.lines:get() - vim.opt.cmdheight:get() - 1
+local height = lines - math.floor(lines / 1.618)
+
 _G.Utils = require("config.utils")
 _G.MainWin = vim.api.nvim_get_current_win()
 _G.PickerPosV = vim.opt.columns:get()
-_G.PickerPosH = vim.opt.lines:get()
+_G.PickerPosH = lines - height - 1
 _G.PickerGoldenV = true
 _G.PickerGoldenH = true
 
