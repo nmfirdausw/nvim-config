d8a028fed266ef5a5c3a0326aaa76880dfdf8a21
nmfirdausw <nmfirdausw@gmail.com>
2025-09-18 21:02:13 +0800

    feat: add window management utility functions
    - Add close_all_floating_win function to close floating windows
    - Add get_split_count function to count window splits

--- lua/config/utils.lua
+++ lua/config/utils.lua
@@ -6,4 +6,22 @@ M.is_git_dir = function()
   return gitdir ~= ""
 end
 
+M.close_all_floating_win = function()
+  local wins = vim.api.nvim_tabpage_list_wins(0)
+  for _, win in ipairs(wins) do
+    if vim.api.nvim_win_is_valid(win) then
+      local config = vim.api.nvim_win_get_config(win)
+      if config.relative ~= "" then
+        vim.api.nvim_win_close(win, true)
+      end
+    end
+  end
+end
+
+M.get_split_count = function()
+  return #vim.tbl_filter(function(win)
+    return vim.api.nvim_win_get_config(win).relative == ""
+  end, vim.api.nvim_tabpage_list_wins(0))
+end
+
 return M
