9a7597a6a3b247350fcd8df56ef41dc230388d74
nmfirdausw <nmfirdausw@gmail.com>
2025-09-18 21:01:53 +0800

    config: add global picker position variables and autocommands
    - Add global variables for picker positioning and golden ratio state
    - Load autocommands configuration module

--- init.lua
+++ init.lua
@@ -1,9 +1,15 @@
 vim.env.PATH = "/opt/homebrew/bin:" .. vim.env.PATH
 
 _G.Utils = require("config.utils")
+_G.MainWin = vim.api.nvim_get_current_win()
+_G.PickerPosV = vim.opt.columns:get()
+_G.PickerPosH = vim.opt.lines:get()
+_G.PickerGoldenV = true
+_G.PickerGoldenH = true
 
 require("config.options")
 require("config.neovide")
 require("config.keymaps")
 require("config.statuscolumn")
+require("config.autocommands")
 require("config.lazy")
