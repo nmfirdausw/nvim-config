6f69fd326892d67e8c8402613fe6bd4348dc6416
nmfirdausw <nmfirdausw@gmail.com>
2025-09-20 01:44:54 +0800

    config: enhance snacks plugin with improved picker configuration
    - Replace inline layout functions with modular picker layouts
    - Add comprehensive keymaps for various picker actions
    - Configure different layouts for specific picker types
    - Optimize layout selection based on content type

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -4,125 +4,127 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons", "nvim-lua/plenary.nvim" },
   keys = {
     {
-      "<BS>",
+      "<leader>s",
       function()
         Utils.close_all_floating_win()
-        Snacks.explorer()
+        Snacks.picker.pickers()
       end,
-      desc = "File Explorer",
+      desc = "Pickers",
     },
     {
       "<leader><leader>",
       function()
         Utils.close_all_floating_win()
-        Snacks.picker.autocmds()
+        Snacks.picker.smart()
+      end,
+      desc = "Smart Find Files",
+    },
+    {
+      "<leader>e",
+      function()
+        Utils.close_all_floating_win()
+        Snacks.picker.explorer()
       end,
       desc = "File Explorer",
     },
+    {
+      "<leader>,",
+      function()
+        Utils.close_all_floating_win()
+        Snacks.picker.buffers()
+      end,
+      desc = "Buffers",
+    },
+    {
+      "<leader>bb",
+      function()
+        Utils.close_all_floating_win()
+        Snacks.picker.buffers()
+      end,
+      desc = "Switch Buffer",
+    },
+    {
+      "<leader>/",
+      function()
+        Utils.close_all_floating_win()
+        Snacks.picker.lines()
+      end,
+      desc = "Lines",
+    },
+    {
+      "<leader>?",
+      function()
+        Utils.close_all_floating_win()
+        Snacks.picker.grep()
+      end,
+      desc = "Grep",
+    },
+    {
+      "<leader>::",
+      function()
+        Utils.close_all_floating_win()
+        Snacks.picker.command_history()
+      end,
+      desc = "Command History",
+    },
+    {
+      "<leader>:",
+      function()
+        Utils.close_all_floating_win()
+        Snacks.picker.commands()
+      end,
+      desc = "Commands",
+    },
+    {
+      "<leader>d",
+      function()
+        Utils.close_all_floating_win()
+        Snacks.picker.diagnostics()
+      end,
+      desc = "Diagnostics",
+    },
+    {
+      "<leader>o",
+      function()
+        Utils.close_all_floating_win()
+        Snacks.picker.jumps()
+      end,
+      desc = "Jumps",
+    },
+    {
+      "<leader>n",
+      function()
+        Utils.close_all_floating_win()
+        Snacks.picker.notifications()
+      end,
+      desc = "Notifications",
+    },
+    {
+      "<leader>l",
+      function()
+        Utils.close_all_floating_win()
+        Snacks.picker.lsp_config()
+      end,
+      desc = "Lsp Config",
+    },
+    {
+      "<leader>P",
+      function()
+        Utils.close_all_floating_win()
+        Snacks.picker.projects()
+      end,
+      desc = "Projects",
+    },
+    {
+      "<leader>r",
+      function()
+        Snacks.picker.recent()
+      end,
+      desc = "Recent",
+    },
   },
   config = function()
-    local vertical = function()
-      local columns = vim.o.columns
-      local main_win_width = vim.api.nvim_win_get_width(MainWin)
-      local main_win_pos = vim.api.nvim_win_get_position(MainWin)
-      local main_win_col = main_win_pos[2]
-      local width = columns - math.floor(columns / 1.618)
-      local border = "left"
-      if not PickerGoldenV then
-        if PickerPosV == 0 then
-          width = main_win_col
-          border = "right"
-        else
-          width = columns - main_win_col - main_win_width
-        end
-      end
-
-      return {
-        preview = "main",
-        layout = {
-          box = "horizontal",
-          width = width,
-          min_width = 1,
-          row = 0,
-          col = PickerPosV,
-          height = vim.o.lines - vim.opt.cmdheight:get() - 1,
-          { win = "preview", title = "{preview}", border = "rounded", width = 0.5 },
-          {
-            box = "vertical",
-            border = border,
-            {
-              win = "input",
-              height = 1,
-              border = "rounded",
-            },
-            { win = "list", border = "none" },
-          },
-        },
-      }
-    end
-
-    -- horizontal function
-    local horizontal = function()
-      local columns = vim.o.columns
-      local lines = vim.o.lines
-      local main_win_height = vim.api.nvim_win_get_height(MainWin)
-      local main_win_pos = vim.api.nvim_win_get_position(MainWin)
-      local main_win_row = main_win_pos[1]
-      local height = lines - math.floor(lines / 1.618)
-      local layout = {
-        preview = "main",
-        layout = {
-          box = "horizontal",
-          width = columns,
-          min_width = 1,
-          row = PickerPosH,
-          col = 0,
-          height = height,
-          { win = "preview", title = "{preview}", border = "rounded", width = 0.5 },
-          {
-            box = "vertical",
-            border = "top",
-            {
-              win = "input",
-              height = 1,
-              border = "bottom",
-            },
-            { win = "list", border = "none" },
-          },
-        },
-      }
-      if not PickerGoldenH then
-        if PickerPosH == 0 then
-          height = main_win_row
-          layout = {
-            preview = "main",
-            reverse = true,
-            layout = {
-              box = "horizontal",
-              width = columns,
-              min_width = 1,
-              row = PickerPosH,
-              col = 0,
-              height = height,
-              { win = "preview", title = "{preview}", border = "rounded", width = 0.5 },
-              {
-                box = "vertical",
-                border = "bottom",
-                { win = "list", border = "none" },
-                {
-                  win = "input",
-                  height = 1,
-                  border = "top",
-                },
-              },
-            },
-          }
-        else
-          height = lines - main_win_row - main_win_height
-        end
-      end
-      return layout
-    end
+    local picker = require("config.picker")
     require("snacks").setup({
       notifier = {
         enabled = false,
@@ -132,7 +134,7 @@ return {
         replace_netrw = true,
       },
       picker = {
-        layout = vertical,
+        layout = picker.layout.vertical_main,
         win = {
           list = {
             wo = {
@@ -159,10 +161,49 @@ return {
             git_status_open = true,
             git_untracked = true,
             auto_close = true,
-            layout = vertical,
+            layout = picker.layout.vertical_main,
           },
-          autocmds = {
-            layout = horizontal,
+          pickers = {
+            layout = picker.layout.vertical,
+          },
+          grep = {
+            layout = picker.layout.horizontal_main,
+          },
+          lines = {
+            layout = picker.layout.horizontal_main,
+          },
+          diagnostics = {
+            layout = picker.layout.horizontal_main,
+          },
+          jumps = {
+            layout = picker.layout.horizontal_main,
+          },
+          commands = {
+            layout = picker.layout.horizontal,
+          },
+          command_history = {
+            layout = picker.layout.horizontal,
+          },
+          lsp_config = {
+            layout = picker.layout.full,
+          },
+          git_log = {
+            layout = picker.layout.horizontal_main,
+          },
+          git_log_line = {
+            layout = picker.layout.horizontal_main,
+          },
+          git_grep = {
+            layout = picker.layout.horizontal_main,
+          },
+          git_stash = {
+            layout = picker.layout.horizontal_main,
+          },
+          git_branches = {
+            layout = picker.layout.horizontal_main,
+          },
+          registers = {
+            layout = picker.layout.horizontal_main,
           },
         },
       },
