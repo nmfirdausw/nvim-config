eb8715df35cdd1e97ae4d1c75492c86b2670fc59
nmfirdausw <nmfirdausw@gmail.com>
2025-09-20 01:44:24 +0800

    refactor: move window management logic to utils module
    - Remove global picker position variables from init.lua
    - Extract golden ratio window resizing logic from autocommands to utils
    - Consolidate window management functions in utils module
    - Simplify autocommands to use centralized utility functions

--- init.lua
+++ init.lua
@@ -5,10 +5,6 @@ local height = lines - math.floor(lines / 1.618)
 
 _G.Utils = require("config.utils")
 _G.MainWin = vim.api.nvim_get_current_win()
-_G.PickerPosV = vim.opt.columns:get()
-_G.PickerPosH = lines - height - 1
-_G.PickerGoldenV = true
-_G.PickerGoldenH = true
 
 require("config.options")
 require("config.neovide")
--- lua/config/autocommands.lua
+++ lua/config/autocommands.lua
@@ -1,66 +1,3 @@
--- Resize splits to golden ratio
-local resize_splits = function(winid)
-  if not vim.g.golden_ratio_splits then
-    return
-  end
-  local columns = vim.opt.columns:get()
-  local lines = vim.opt.lines:get() - vim.opt.cmdheight:get() - 1
-  local win_config = vim.api.nvim_win_get_config(winid)
-  if win_config.relative ~= "" then
-    return
-  end
-  local width = math.floor(columns / 1.618)
-  local height = math.floor(lines / 1.618)
-  if vim.api.nvim_win_get_width(winid) ~= columns then
-    vim.api.nvim_win_set_width(winid, width)
-  end
-  if vim.api.nvim_win_get_height(winid) ~= lines then
-    vim.api.nvim_win_set_height(winid, height)
-  end
-end
-
-local set_picker_vertical_win = function(winid)
-  local main_win_width = vim.api.nvim_win_get_width(winid)
-  local columns = vim.opt.columns:get()
-  local width = columns - math.floor(columns / 1.618)
-  PickerPosV = columns
-
-  if main_win_width == columns then
-    PickerGoldenV = true
-  else
-    local main_win_pos = vim.api.nvim_win_get_position(winid)
-    local main_win_col = main_win_pos[2]
-    width = columns - main_win_col - main_win_width
-    PickerGoldenV = false
-    if width < main_win_col then
-      PickerPosV = 0
-    else
-      PickerPosV = columns
-    end
-  end
-end
-
-local set_picker_horizontal_win = function(winid)
-  local main_win_height = vim.api.nvim_win_get_height(winid)
-  local lines = vim.opt.lines:get() - vim.opt.cmdheight:get() - 1
-  local height = lines - math.floor(lines / 1.618)
-  PickerPosH = lines - height - 1
-
-  if main_win_height == lines then
-    PickerGoldenH = true
-  else
-    local main_win_pos = vim.api.nvim_win_get_position(winid)
-    local main_win_row = main_win_pos[1]
-    height = lines - main_win_row - main_win_height
-    PickerGoldenH = false
-    if height < main_win_row then
-      PickerPosH = 0
-    else
-      PickerPosH = lines - height - 1
-    end
-  end
-end
-
 vim.api.nvim_create_autocmd({ "WinLeave", "WinEnter" }, {
   callback = function()
     if vim.bo.buftype ~= "" then
@@ -70,9 +7,7 @@ vim.api.nvim_create_autocmd({ "WinLeave", "WinEnter" }, {
     local main_win = vim.api.nvim_get_current_win()
 
     MainWin = main_win
-    resize_splits(MainWin)
-    set_picker_vertical_win(MainWin)
-    set_picker_horizontal_win(MainWin)
+    Utils.resize_splits(MainWin)
   end,
 })
 
@@ -86,8 +21,6 @@ vim.api.nvim_create_autocmd({ "WinLeave", "WinEnter" }, {
 
 vim.api.nvim_create_autocmd("VimResized", {
   callback = function()
-    resize_splits(MainWin)
-    set_picker_vertical_win(MainWin)
-    set_picker_horizontal_win(MainWin)
+    Utils.resize_splits(MainWin)
   end,
 })
