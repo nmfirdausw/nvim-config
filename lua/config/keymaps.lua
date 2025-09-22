97c228ffc86652d51e096357141693a01c982a4b
nmfirdausw <nmfirdausw@gmail.com>
2025-09-22 23:06:15 +0800

    refactor: simplify keymaps configuration
    - Remove redundant clipboard overrides for y/Y/p/P
    - Add visual mode indent keymaps that maintain selection
    - Keep existing Ctrl+C clipboard shortcuts and navigation improvements

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,14 +1,5 @@
 vim.g.mapleader = " "
 
-vim.keymap.set("n", "y", "\"+y", { desc = "Yank" })
-vim.keymap.set("v", "y", "\"+y", { desc = "Yank" })
-vim.keymap.set("n", "Y", "\"+Y", { desc = "Yank line" })
-
-vim.keymap.set("n", "p", "\"+p", { desc = "Paste" })
-vim.keymap.set("v", "p", "\"+p", { desc = "Paste" })
-vim.keymap.set("n", "P", "\"+P", { desc = "Paste before" })
-vim.keymap.set("v", "P", "\"+P", { desc = "Paste before" })
-
 vim.keymap.set("n", "<C-c>", "\"+yy", { desc = "Copy line to clipboard" })
 vim.keymap.set("v", "<C-c>", "\"+y", { desc = "Copy to clipboard" })
 
@@ -19,3 +10,6 @@ vim.keymap.set("n", "<S-right>", "w")
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
