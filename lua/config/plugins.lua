8f52d41b07c1bcf6d4374da85268f2af0a455bc1
nmfirdausw <nmfirdausw@gmail.com>
2025-09-23 16:24:18 +0800

    feat: add which-key plugin and conditional gitsigns loading
    - Add which-key.nvim plugin for keymap help
    - Conditionally load gitsigns only in git repositories
    - Use Utils.is_git_dir() to check for git directory before loading gitsigns

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -7,4 +7,9 @@ vim.pack.add({
   { src = "https://github.com/rachartier/tiny-inline-diagnostic.nvim" },
   { src = "https://github.com/stevearc/conform.nvim" },
   { src = "https://github.com/nvim-mini/mini.ai" },
+  { src = "https://github.com/folke/which-key.nvim" },
 })
+
+if Utils.is_git_dir() then
+  vim.pack.add({ "https://github.com/lewis6991/gitsigns.nvim" })
+end
