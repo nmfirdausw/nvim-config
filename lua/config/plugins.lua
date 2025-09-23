088c73c82c52df35d0456f313f98c7dad928e499
nmfirdausw <nmfirdausw@gmail.com>
2025-09-23 13:51:19 +0800

    feat: update plugin configuration
    - Add tiny-inline-diagnostic, conform.nvim, and mini.ai plugins
    - Comment out copilot-lsp in favor of copilot.lua
    - Organize plugin list for better maintainability

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -2,6 +2,9 @@ vim.pack.add({
   { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
   { src = "https://github.com/neovim/nvim-lspconfig" },
   { src = "https://github.com/mason-org/mason.nvim" },
-  { src = "https://github.com/copilotlsp-nvim/copilot-lsp" },
+  -- { src = "https://github.com/copilotlsp-nvim/copilot-lsp" },
   { src = "https://github.com/zbirenbaum/copilot.lua" },
+  { src = "https://github.com/rachartier/tiny-inline-diagnostic.nvim" },
+  { src = "https://github.com/stevearc/conform.nvim" },
+  { src = "https://github.com/nvim-mini/mini.ai" },
 })
