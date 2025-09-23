a2339c65472d55ca9f9050b3d40bba1542940644
nmfirdausw <nmfirdausw@gmail.com>
2025-09-23 13:50:53 +0800

    feat: add which-key for keybinding discovery
    - Configure which-key with rounded border matching global settings
    - Set up keybinding groups for toggle and buffer operations
    - Disable mapping icons for cleaner interface

---
+++ lua/config/which-key.lua
@@ -0,0 +1,22 @@
+vim.api.nvim_create_autocmd("VimEnter", {
+  group = vim.api.nvim_create_augroup("mini_config", { clear = true }),
+  callback = function()
+    vim.schedule(function()
+      require("which-key").setup({
+        win = {
+          no_overlap = true,
+          col = 0,
+          border = vim.g.border,
+        },
+        icons = {
+          mappings = false,
+        },
+        spec = {
+          { "<leader>t", desc = "Toggle" },
+          { "<leader>b", desc = "Buffer" },
+        },
+      })
+    end)
+  end,
+  once = true,
+})
