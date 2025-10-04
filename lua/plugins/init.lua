e3961aa83cc54f2fc7ffaa8b3610ff8cdbeb9e5d
nmfirdausw <nmfirdausw@gmail.com>
2025-10-05 01:47:16 +0800

    refactor: update plugin initialization order
    - Move snacks.nvim earlier in load order
    - Move which-key.nvim before mini.ai
    - Remove blink.pairs from plugin list

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -3,15 +3,15 @@ vim.pack.add({
   "https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim",
   { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
   "https://github.com/stevearc/conform.nvim",
+  "https://github.com/folke/snacks.nvim",
   "https://github.com/kevinhwang91/promise-async",
   "https://github.com/kevinhwang91/nvim-ufo",
+  "https://github.com/folke/which-key.nvim",
   "https://github.com/nvim-mini/mini.ai",
   { src = "https://github.com/Saghen/blink.cmp", version = "main" },
-  { src = "https://github.com/Saghen/blink.pairs", version = "main" },
   "https://github.com/lewis6991/gitsigns.nvim",
   "https://github.com/rachartier/tiny-inline-diagnostic.nvim",
   "https://github.com/nvim-tree/nvim-web-devicons",
-  "https://github.com/folke/snacks.nvim",
 }, {
   confirm = false,
   load = function(plugin)
